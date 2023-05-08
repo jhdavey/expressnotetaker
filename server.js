be2c3999cab995d77d5a026b5916a660bd5778b7
jhdavey <jhdavey5182@gmail.com>
2023-05-07 20:50:07 -0400

    Updated paths to get and post notes, started working on deleting notes.

--- server.js
+++ server.js
@@ -1,36 +1,62 @@
-const PORT = process.env.PORT || 3001;
+const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const uuid = require('uuid');
 
-const express = require('express');
+//Designate PORT
+const PORT = process.env.PORT || 3001;
+
+//Create instance of express
 const app = express();
 
-const notes = require('./db/db.json');
-
+//Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 //API routes
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
-});
 
-app.post('/api/notes', (req, res) => {
-    res.json(notes);
-});
-
-app.delete('/api/notes/:id', (req, res) => {
-    deleteNote(req.params.id, notes);
-    res.json(true);
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
+//Return notes page when requested
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+//Get existing notes from database
+app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, './db/db.json'), 'utf-8', (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        res.json(notes);
+    })
+});
+
+//Post new notes to database
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    newNote.id = uuid.v4();
+    fs.readFile(path.join(__dirname, './db/db.json'), 'utf-8', (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        notes.push(newNote);
+        fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(notes), (err) => {
+            if (err) throw err;
+            res.json(newNote);
+        })
+    })
+});
+
+//Delete notes from database
+app.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+    fs.readFile(path.join(__dirname, './db/db.json'), 'utf-8', (err, data) => {
+        if (err) throw err;
+        let notes = JSON.parse(data);
+        notes = notes.filter(note => note.id !== note.Id);
+        fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(notes), (err) => {
+            if (err) throw err;
+            res.json({ id: noteId });
+        })
+    })
 });
 
 app.get('*', (req, res) => {
